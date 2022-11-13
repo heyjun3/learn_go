56c99ec4b012b84cfff7660e67d3449216dedb93
heyjun3 <t3k2g5kako@yahoo.co.jp>
2022-11-13 19:04:40 +0000

    done define

--- helloworld.go
+++ helloworld.go
@@ -7,7 +7,68 @@ import (
 )
 
 func main() {
-	list()
+	defineQ1()
+}
+
+func defineQ1() {
+	f := 1.11
+	fmt.Println(int(f))
+	m := map[string]int{"Mike": 20, "Nancy": 24, "Messi": 30}
+	fmt.Printf("%T %v", m, m)
+}
+
+func args(params ...int) {
+	fmt.Println(len(params), params)
+}
+
+
+func counter() (func() int) {
+	x := 0
+	return func() int{
+		x++
+		return x
+	}
+}
+
+func circleArea(pi float64) func(radius float64) float64 {
+	return func(radius float64) float64 {
+		return pi * radius * radius
+	}
+}
+
+func add(x int, y int) (int, int){
+	return x + y, x * y
+}
+
+func create() {
+	f := func(x int) {
+		fmt.Println("inner func", x)
+	}
+	fmt.Printf("%T", f)
+	f(2)
+}
+
+func bytes() {
+	b := []byte{72, 73}
+	fmt.Println(b)
+	fmt.Println(string(b))
+
+	c := []byte("HI")
+	fmt.Println(c)
+	fmt.Println(string(c))
+}
+
+func dict() {
+	m := map[string]int{"apple": 100, "banana": 200}
+	fmt.Println(m)
+	fmt.Println(m["apple"])
+	fmt.Println(m["nothing"])
+
+	v, ok := m["apple"]
+	fmt.Println(v, ok)
+
+	v2, ok2 := m["nothing"]
+	fmt.Println(v2, ok2)
 }
 
 func hello() {
