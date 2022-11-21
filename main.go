d34c355fd470d004f8ba6e7d06868df95dd38414
heyjun3 <t3k2g5kako@yahoo.co.jp>
2022-11-21 14:27:38 +0000

    add go

--- main.go
+++ main.go
@@ -1,12 +1,52 @@
 package main
 
 import (
+	"context"
 	"fmt"
-	"myproject/mylib"
+	"regexp"
+	"time"
 )
 
 func main() {
-	s := []int{1, 2, 3, 4, 5}
-	avg := mylib.Average(s)
-	fmt.Println(avg)
+	run()
+}
+
+func longProcess(ctx context.Context, ch chan string) {
+	fmt.Println("run")
+	time.Sleep(2 * time.Second)
+	fmt.Println("finish")
+	ch <- "result"
+}
+
+func run() {
+	ch := make(chan string)
+	ctx := context.Background()
+	ctx, cancel := context.WithTimeout(ctx, 1 * time.Second)
+	defer cancel()
+	go longProcess(ctx, ch)
+	for {
+		select {
+		case <- ctx.Done():
+			fmt.Println(ctx.Err())
+			return
+		case  <- ch:
+			fmt.Println("success")
+			return
+		}
+	}
+}
+
+func match() {
+	m, _ := regexp.MatchString("a([a-z]+)e", "apple")
+	fmt.Println(m)
+
+	r := regexp.MustCompile("a([a-z]+)e")
+	ms := r.MatchString("apple")
+	fmt.Println(ms)
+}
+func timeer() {
+	t := time.Now()
+	fmt.Println(t)
+	fmt.Println(t.Format(time.RFC3339))
+	fmt.Println(t.Year(), t.Month(), t.Day(), t.Hour(), t.Minute(), t.Second())
 }
