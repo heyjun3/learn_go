0307baf486999cd319d1a93c9a5f06f29a2146bd
heyjun3 <t3k2g5kako@yahoo.co.jp>
2022-12-19 14:16:43 +0000

    add get ticker

--- trading/main.go
+++ trading/main.go
@@ -1,10 +1,19 @@
 package main
 
 import (
+	"fmt"
+	"time"
 	"trading/config"
 	"trading/utils"
+	"trading/bitflyer"
 )
 
 func main() {
 	utils.LoggingSettings(config.Config.LogFile)
+	apiClient := bitflyer.New(config.Config.ApiKey, config.Config.ApiSecret)
+	ticker, _ := apiClient.GetTicker("BTC_USD")
+	fmt.Println(ticker)
+	fmt.Println(ticker.GetMidPrice())
+	fmt.Println(ticker.DateTime())
+	fmt.Println(ticker.TruncateDateTime(time.Hour))
 }
