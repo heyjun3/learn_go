28c3c251169d2eaa552934f388ed20d6995fcf76
heyjun3 <t3k2g5kako@yahoo.co.jp>
2022-12-25 21:16:41 +0900

    add web server

--- trading/main.go
+++ trading/main.go
@@ -2,14 +2,12 @@ package main
 
 import (
 	"trading/app/controllers"
-	"trading/app/models"
-	"fmt"
 	"trading/config"
 	"trading/utils"
 )
 
 func main() {
 	utils.LoggingSettings(config.Config.LogFile)
-	fmt.Println(models.DbConnection)
 	controllers.StreamIngestionData()
+	controllers.StartWebServer()
 }
