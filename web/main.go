f4b99a7a11a36832c0650420243befd6dfb43a59
heyjun3 <t3k2g5kako@yahoo.co.jp>
2023-04-08 11:19:25 +0900

    fix

--- web/main.go
+++ web/main.go
@@ -1,13 +1,15 @@
 package main
 
-
 import (
+	"context"
 	"fmt"
 	"net/http"
+	"os/exec"
 )
 
 func main() {
-	StartServer()
+	ctx := context.Background()
+	runJobs(ctx)
 }
 
 func Hello(w http.ResponseWriter, r *http.Request) {
@@ -33,4 +35,34 @@ func patch(fn func(w http.ResponseWriter, r *http.Request)) http.HandlerFunc {
 func StartServer() {
 	http.HandleFunc("/", patch(log(Hello)))
 	http.ListenAndServe(":8080", nil)
+}
+
+func runJobs(ctx context.Context) error {
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+	ec := make(chan error)
+	done := make(chan struct{})
+
+	for i := 0; i < 10; i++ {
+		go func() {
+			cmd := exec.CommandContext(ctx, "sleep", "30")
+			err := cmd.Run()
+			if err != nil {
+				ec <- err
+			} else {
+				done <- struct{}{}
+			}
+		}()
+	}
+
+	for i := 0; i < 10; i++ {
+		select {
+		case err := <-ec:
+			return err
+		case <-done:
+			return nil
+		}
+	}
+	return nil
 }
